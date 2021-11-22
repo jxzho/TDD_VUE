9e7d7e65f4a790a6884202af21cfa187be2757fa
Dell <dellzhong@xiaoman.cn>
2021-11-22 21:14:01 +0800

    feat: init

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  preset: "@vue/cli-plugin-unit-jest",
+
+  moduleFileExtensions: ["js", "jsx", "vue", "json"],
+
+  transform: {
+    "^.+\\.vue": "vue-jest",
+  },
+
+  transformIgnorePatterns: ["/node_modules/"],
+
+  moduleNameMapper: {
+    "/^@/(.*)$/": "<rootDir>/src/$1",
+  },
+
+  snapshotSerializers: ["jest-serializer-vue"],
+
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+
+  testURL: "http://localhost",
+
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+};
