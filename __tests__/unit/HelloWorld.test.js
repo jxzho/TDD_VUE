6575b9a0a92a6f63a391bb86ecf253757fc8adc6
Dell <dellzhong@xiaoman.cn>
2021-11-23 10:09:47 +0800

    test: use shallowMount

---
+++ __tests__/unit/HelloWorld.test.js
@@ -0,0 +1,17 @@
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+// shallowMount 浅渲染，渲染一层，单例测试
+// mount 子组件也渲染，集成测试
+
+// 4-3
