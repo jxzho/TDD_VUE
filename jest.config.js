4a6360fab4bdcc45f4a5cedc0d2a1e4de355ac4f
Dell <dellzhong@xiaoman.cn>
2021-11-23 10:07:41 +0800

    chore: some

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,8 @@ module.exports = {
 
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
 
+  testPathIgnorePatterns: [],
+
   testURL: "http://localhost",
 
   watchPlugins: [
